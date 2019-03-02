ff6ee60d18fe9180efc3140414740f50f302ec76
eballo <enric.ballo@gmail.com>
2019-03-02 20:35:40 +0100

    Swagger only rest points from the specific package

--- mastermind-app/src/main/java/com/bluecodex/java/mastermind/config/SwaggerConfig.java
+++ mastermind-app/src/main/java/com/bluecodex/java/mastermind/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bluecodex.java.mastermind.service.rest"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
